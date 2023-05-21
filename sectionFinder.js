9f08ee4233c2c9ec4399ba1c3752e1a53f618a51
Tony Thomas <tony.thomas@schibsted.com>
2023-05-21 12:57:50 +0300

    Fetch content before len

--- sectionFinder.js
+++ sectionFinder.js
@@ -41,7 +41,7 @@ function formatSection(section) {
     const namespace = mw.config.get("wgCanonicalNamespace");
     if (namespace === "Talk") {
         // We do not store the data.
-        section.contentPlainLength = getPlainTextFromHtml(section.contentHtml).contentPlain.length;
+        section.contentPlainLength = getPlainTextFromHtml(section.contentHtml).length;
     } else {
         section.contentPlain = getPlainTextFromHtml(section.contentHtml);
         section.contentPlainLength = section.contentPlain.length;
