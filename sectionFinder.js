d720ee996af7880ea3019c4703a93535427982e4
Tony Thomas <tony.thomas@schibsted.com>
2023-06-23 15:13:50 +0200

    House keeping: husky, pre-commits, prettier

--- sectionFinder.js
+++ sectionFinder.js
@@ -1,173 +1,179 @@
 function getSections() {
-    // We do not require section data scrapped for Talk pages.
+  // We do not require section data scrapped for Talk pages.
 
-    const skin = mw.config.get('skin');
-    const namespace = mw.config.get('wgCanonicalNamespace');
+  const skin = mw.config.get("skin");
+  const namespace = mw.config.get("wgCanonicalNamespace");
 
-    let sections = [];
-    if (namespace === "Talk") {
-        sections = findSectionInTalkPage()
-    } else if (skin === "vector-2022" && namespace === "") {
-        sections = findSectionsVector2022InArticlePage()
-    } else if (skin === "minerva" && namespace === "") {
-        sections = findSectionsMinervaInArticlePage()
-    } else {
-        console.error('Section Summarizer does not support this skin or namespace');
-        return;
-    }
+  let sections = [];
+  if (namespace === "Talk") {
+    sections = findSectionInTalkPage();
+  } else if (skin === "vector-2022" && namespace === "") {
+    sections = findSectionsVector2022InArticlePage();
+  } else if (skin === "minerva" && namespace === "") {
+    sections = findSectionsMinervaInArticlePage();
+  } else {
+    console.error("Section Summarizer does not support this skin or namespace");
+    return;
+  }
 
-    if (sections.length !== 0) {
-        sections = sections.map(formatSection);
-    }
-    return sections;
+  if (sections.length !== 0) {
+    sections = sections.map(formatSection);
+  }
+  return sections;
 }
 
 function getPlainTextFromHtml(html) {
-    var temp = document.createElement('div');
-    temp.innerHTML = html;
+  var temp = document.createElement("div");
+  temp.innerHTML = html;
 
-    // Remove content of <style>, <script>, and other similar elements
-    var elementsToRemove = temp.querySelectorAll('style, script, noscript, iframe, object, embed, span.mw-editsection, img');
-    Array.prototype.forEach.call(elementsToRemove, function (element) {
-        element.parentNode.removeChild(element);
-    });
+  // Remove content of <style>, <script>, and other similar elements
+  var elementsToRemove = temp.querySelectorAll(
+    "style, script, noscript, iframe, object, embed, span.mw-editsection, img"
+  );
+  Array.prototype.forEach.call(elementsToRemove, function (element) {
+    element.parentNode.removeChild(element);
+  });
 
-    var plainText = temp.textContent || temp.innerText || '';
+  var plainText = temp.textContent || temp.innerText || "";
 
-    return plainText.trim();
+  return plainText.trim();
 }
 
 function formatSection(section) {
-    const namespace = mw.config.get("wgCanonicalNamespace");
-    if (namespace !== "Talk") {
-        section.contentPlain = getPlainTextFromHtml(section.contentHtml);
-        section.contentPlainLength = section.contentPlain.length;
-    }
-    return section;
+  const namespace = mw.config.get("wgCanonicalNamespace");
+  if (namespace !== "Talk") {
+    section.contentPlain = getPlainTextFromHtml(section.contentHtml);
+    section.contentPlainLength = section.contentPlain.length;
+  }
+  return section;
 }
-
 
 /* Minerva skin */
 function findSectionsMinervaInTalkPage() {
-    headingElements = document.querySelectorAll('#mw-content-text > .mw-parser-output > .mw-heading');
-    if (headingElements.length === 0) {
-        console.error('can not find heading elements');
-        return null;
+  headingElements = document.querySelectorAll(
+    "#mw-content-text > .mw-parser-output > .mw-heading"
+  );
+  if (headingElements.length === 0) {
+    console.error("can not find heading elements");
+    return null;
+  }
+
+  const sections = [];
+  headingElements.forEach(function (headingElement) {
+    const section = {};
+
+    const titleElement = headingElement.querySelector(".mw-headline");
+    if (!titleElement) {
+      console.error("can not find title element");
+      return null;
+    }
+    section.title = titleElement.textContent;
+
+    const sectionElement = headingElement.nextElementSibling;
+    if (!sectionElement || sectionElement.tagName !== "SECTION") {
+      console.error("can not find section element");
+      return null;
     }
 
-    const sections = [];
-    headingElements.forEach(function (headingElement) {
-        const section = {};
+    section.contentHtml = sectionElement.outerHTML;
+    section.firstContentElement = sectionElement.firstElementChild;
 
-        const titleElement = headingElement.querySelector('.mw-headline');
-        if (!titleElement) {
-            console.error('can not find title element');
-            return null;
-        }
-        section.title = titleElement.textContent;
+    sections.push(section);
+  });
 
-        const sectionElement = headingElement.nextElementSibling;
-        if (!sectionElement || sectionElement.tagName !== 'SECTION') {
-            console.error('can not find section element');
-            return null;
-        }
-
-        section.contentHtml = sectionElement.outerHTML;
-        section.firstContentElement = sectionElement.firstElementChild;
-
-        sections.push(section);
-    });
-
-    return sections;
+  return sections;
 }
 
-
 function findSectionsMinervaInArticlePage() {
-    headingElements = document.querySelectorAll('#mw-content-text > .mw-parser-output > h2.section-heading');
-    if (headingElements.length === 0) {
-        console.error('can not find heading elements');
-        return null;
+  headingElements = document.querySelectorAll(
+    "#mw-content-text > .mw-parser-output > h2.section-heading"
+  );
+  if (headingElements.length === 0) {
+    console.error("can not find heading elements");
+    return null;
+  }
+
+  const sections = [];
+  headingElements.forEach(function (headingElement) {
+    const section = {};
+
+    const titleElement = headingElement.querySelector(".mw-headline");
+    if (!titleElement) {
+      console.error("can not find title element");
+      return null;
+    }
+    section.title = titleElement.textContent;
+
+    const sectionElement = headingElement.nextElementSibling;
+    if (!sectionElement || sectionElement.tagName !== "SECTION") {
+      console.error("can not find section element");
+      return null;
     }
 
-    const sections = [];
-    headingElements.forEach(function (headingElement) {
-        const section = {};
+    section.contentHtml = sectionElement.outerHTML;
+    section.firstContentElement = sectionElement.firstElementChild;
 
-        const titleElement = headingElement.querySelector('.mw-headline');
-        if (!titleElement) {
-            console.error('can not find title element');
-            return null;
-        }
-        section.title = titleElement.textContent;
+    sections.push(section);
+  });
 
-        const sectionElement = headingElement.nextElementSibling;
-        if (!sectionElement || sectionElement.tagName !== 'SECTION') {
-            console.error('can not find section element');
-            return null;
-        }
-
-        section.contentHtml = sectionElement.outerHTML;
-        section.firstContentElement = sectionElement.firstElementChild;
-
-        sections.push(section);
-    });
-
-    return sections;
+  return sections;
 }
 
 /* Vector 2022 */
 function findSectionInTalkPage() {
-    const sections = [];
-    $('#mw-content-text > .mw-parser-output > .mw-heading').each(function() {
-        const titleElement = $(this).find('h2 > .mw-headline');
-        if (!titleElement.length) {
-            console.error('Cannot find title element');
-            return null;
-        }
-        sections.push({
-            contentPlainLength: $(this).nextUntil('.mw-heading').text().length,
-            title: titleElement.text(),
-            firstContentElement: this.nextElementSibling
-        });
+  const sections = [];
+  $("#mw-content-text > .mw-parser-output > .mw-heading").each(function () {
+    const titleElement = $(this).find("h2 > .mw-headline");
+    if (!titleElement.length) {
+      console.error("Cannot find title element");
+      return null;
+    }
+    sections.push({
+      contentPlainLength: $(this).nextUntil(".mw-heading").text().length,
+      title: titleElement.text(),
+      firstContentElement: this.nextElementSibling,
     });
+  });
 
-    return sections;
+  return sections;
 }
 
 function findSectionsVector2022InArticlePage() {
-    headingElements = document.querySelectorAll('#mw-content-text > .mw-parser-output > h2');
-    if (headingElements.length === 0) {
-        console.error('can not find heading elements');
-        return null;
+  headingElements = document.querySelectorAll(
+    "#mw-content-text > .mw-parser-output > h2"
+  );
+  if (headingElements.length === 0) {
+    console.error("can not find heading elements");
+    return null;
+  }
+
+  const sections = [];
+  headingElements.forEach(function (headingElement) {
+    const section = {};
+
+    const titleElement = headingElement.querySelector(".mw-headline");
+    if (!titleElement) {
+      console.error("can not find title element");
+      return null;
+    }
+    section.title = titleElement.textContent;
+
+    section.contentHtml = "";
+    let nextElement = headingElement.nextElementSibling;
+    let firstContentElementFound = false;
+
+    while (nextElement && nextElement.tagName !== "H2") {
+      if (!firstContentElementFound) {
+        section.firstContentElement = nextElement;
+        firstContentElementFound = true;
+      }
+
+      section.contentHtml += nextElement.outerHTML;
+      nextElement = nextElement.nextElementSibling;
     }
 
-    const sections = [];
-    headingElements.forEach(function (headingElement) {
-        const section = {};
+    sections.push(section);
+  });
 
-        const titleElement = headingElement.querySelector('.mw-headline');
-        if (!titleElement) {
-            console.error('can not find title element');
-            return null;
-        }
-        section.title = titleElement.textContent;
-
-        section.contentHtml = '';
-        let nextElement = headingElement.nextElementSibling;
-        let firstContentElementFound = false;
-
-        while (nextElement && nextElement.tagName !== 'H2') {
-            if (!firstContentElementFound) {
-                section.firstContentElement = nextElement;
-                firstContentElementFound = true;
-            }
-
-            section.contentHtml += nextElement.outerHTML;
-            nextElement = nextElement.nextElementSibling;
-        }
-
-        sections.push(section);
-    });
-
-    return sections;
+  return sections;
 }
--- settings.js
+++ settings.js
@@ -1,65 +1,65 @@
 // Make a subclass of ProcessDialog
 function SectionSummarizerSettingsDialog(config) {
-    SectionSummarizerSettingsDialog.super.call(this, config);
+  SectionSummarizerSettingsDialog.super.call(this, config);
 }
 OO.inheritClass(SectionSummarizerSettingsDialog, OO.ui.ProcessDialog);
 
-SectionSummarizerSettingsDialog.static.name = 'sectionSummarizerSettings';
-SectionSummarizerSettingsDialog.static.title = 'Summarizer Settings';
+SectionSummarizerSettingsDialog.static.name = "sectionSummarizerSettings";
+SectionSummarizerSettingsDialog.static.title = "Summarizer Settings";
 SectionSummarizerSettingsDialog.static.actions = [
-    {
-        flags: ['primary', 'progressive'],
-        label: 'Save',
-        action: 'save',
-    },
-    {
-        flags: 'safe',
-        label: 'Cancel',
-    },
+  {
+    flags: ["primary", "progressive"],
+    label: "Save",
+    action: "save",
+  },
+  {
+    flags: "safe",
+    label: "Cancel",
+  },
 ];
 
 // Customize the initialize() function to add content and layouts:
 SectionSummarizerSettingsDialog.prototype.initialize = function () {
-    SectionSummarizerSettingsDialog.super.prototype.initialize.call(this);
-    this.panel = new OO.ui.PanelLayout({
-        padded: true,
-        expanded: false,
+  SectionSummarizerSettingsDialog.super.prototype.initialize.call(this);
+  this.panel = new OO.ui.PanelLayout({
+    padded: true,
+    expanded: false,
+  });
+
+  this.content = new OO.ui.FieldsetLayout();
+
+  this.aiEngineRadioSelect = new OO.ui.RadioSelectWidget();
+
+  this.aiEngineOptions = [
+    { data: "gpt-4", label: "OpenAI GPT-4" },
+    { data: "gpt-3.5-turbo", label: "OpenAI GPT-3.5" },
+  ];
+
+  this.aiEngineOptions.forEach((option) => {
+    const radioOption = new OO.ui.RadioOptionWidget({
+      data: option.data,
+      label: option.label,
     });
+    this.aiEngineRadioSelect.addItems([radioOption]);
+  });
 
-    this.content = new OO.ui.FieldsetLayout();
+  this.aiEngineField = new OO.ui.FieldLayout(this.aiEngineRadioSelect, {
+    label: "AI Engine",
+    align: "top",
+  });
 
-    this.aiEngineRadioSelect = new OO.ui.RadioSelectWidget();
+  this.apiKeyInput = new OO.ui.TextInputWidget({
+    placeholder: "Enter your OpenAI API key",
+  });
 
-    this.aiEngineOptions = [
-        { data: 'gpt-4', label: 'OpenAI GPT-4' },
-        { data: 'gpt-3.5-turbo', label: 'OpenAI GPT-3.5' },
-    ];
+  this.apiKeyField = new OO.ui.FieldLayout(this.apiKeyInput, {
+    label: "OpenAI API Key",
+    align: "top",
+  });
 
-    this.aiEngineOptions.forEach((option) => {
-        const radioOption = new OO.ui.RadioOptionWidget({
-            data: option.data,
-            label: option.label,
-        });
-        this.aiEngineRadioSelect.addItems([radioOption]);
-    });
-
-    this.aiEngineField = new OO.ui.FieldLayout(this.aiEngineRadioSelect, {
-        label: 'AI Engine',
-        align: 'top',
-    });
-
-    this.apiKeyInput = new OO.ui.TextInputWidget({
-        placeholder: 'Enter your OpenAI API key',
-    });
-
-    this.apiKeyField = new OO.ui.FieldLayout(this.apiKeyInput, {
-        label: 'OpenAI API Key',
-        align: 'top',
-    });
-
-    this.content.addItems([this.aiEngineField, this.apiKeyField]);
-    this.panel.$element.append(this.content.$element);
-    this.$body.append(this.panel.$element);
+  this.content.addItems([this.aiEngineField, this.apiKeyField]);
+  this.panel.$element.append(this.content.$element);
+  this.$body.append(this.panel.$element);
 };
 
 // Specify the dialog height (or don't to use the automatically generated height).
@@ -70,41 +70,37 @@ SectionSummarizerSettingsDialog.prototype.initialize = function () {
 // Use getSetupProcess() to set up the window with data passed to it at the time
 // of opening (e.g., url: 'http://www.mediawiki.org', in this example).
 SectionSummarizerSettingsDialog.prototype.getSetupProcess = function (data) {
-    data = data || {};
-    return SectionSummarizerSettingsDialog.super.prototype.getSetupProcess.call(this, data).next(
-        function () {
-            // Set up contents based on data
-            this.aiEngineRadioSelect.selectItemByData(
-                localStorage.getItem('mw_summarizer_aiEngine') || 'gpt-3.5'
-            );
-            this.apiKeyInput.setValue(
-                localStorage.getItem('mw_summarizer_apiKey') || ''
-            );
-        },
-        this
-    );
+  data = data || {};
+  return SectionSummarizerSettingsDialog.super.prototype.getSetupProcess
+    .call(this, data)
+    .next(function () {
+      // Set up contents based on data
+      this.aiEngineRadioSelect.selectItemByData(
+        localStorage.getItem("mw_summarizer_aiEngine") || "gpt-3.5"
+      );
+      this.apiKeyInput.setValue(
+        localStorage.getItem("mw_summarizer_apiKey") || ""
+      );
+    }, this);
 };
 
 // Specify processes to handle the actions.
 SectionSummarizerSettingsDialog.prototype.getActionProcess = function (action) {
-    if (action === 'save') {
-        // Create a new process to handle the action
-        return new OO.ui.Process(
-            function () {
-                localStorage.setItem(
-                    'openAiKey',
-                    this.aiEngineRadioSelect.getSelectedItem().getData()
-                );
-                localStorage.setItem(
-                    'openAiKey',
-                    this.apiKeyInput.getValue()
-                );
-            },
-            this
-        );
-    }
-    // Fallback to parent handler
-    return SectionSummarizerSettingsDialog.super.prototype.getActionProcess.call(this, action);
+  if (action === "save") {
+    // Create a new process to handle the action
+    return new OO.ui.Process(function () {
+      localStorage.setItem(
+        "openAiKey",
+        this.aiEngineRadioSelect.getSelectedItem().getData()
+      );
+      localStorage.setItem("openAiKey", this.apiKeyInput.getValue());
+    }, this);
+  }
+  // Fallback to parent handler
+  return SectionSummarizerSettingsDialog.super.prototype.getActionProcess.call(
+    this,
+    action
+  );
 };
 
 // Create and append a window manager.
